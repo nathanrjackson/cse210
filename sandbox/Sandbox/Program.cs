6ab6d3f9d5b6aa0748cb26f59fc56f279711d934
Nathan Jackson <nathanrjackson30@gmail.com>
2025-01-09 19:52:22 -0700

    Class notes and README

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        
+        for (int i = 1; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        string name = "";
+        Console.Write("Enter first name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine(name);
     }
 }
