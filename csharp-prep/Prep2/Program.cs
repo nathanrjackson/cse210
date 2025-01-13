142b779dd0710d850f43f0009d41ddaf117b8356
Nathan Jackson <nathanrjackson30@gmail.com>
2025-01-13 09:47:55 -0700

    Completed preps 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade percentage: ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+        int secondDigit = gradePercentage % 10;
+
+        string gradeModifier = "";
+
+        if (secondDigit >= 7)
+        {
+            gradeModifier = "+";
+        }
+        else if (secondDigit < 3)
+        {
+            gradeModifier = "-";
+        }
+
+        string letterGrade = "";
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (gradePercentage < 60)
+        {
+            letterGrade = "F";
+        }
+
+        if (!(gradeModifier == "") && letterGrade != "A" && letterGrade != "F")
+        {
+            Console.WriteLine($"Letter grade: {letterGrade}{gradeModifier}");
+        }
+        else if (letterGrade == "A")
+        {
+            if (gradeModifier == "+") {
+                Console.WriteLine($"Letter grade: {letterGrade}");
+            }
+            else
+            {
+                Console.WriteLine($"Letter grade: {letterGrade}{gradeModifier}");
+            }
+        }
+        
+        else
+        {
+            Console.WriteLine($"Letter grade: {letterGrade}");
+        }
     }
 }
