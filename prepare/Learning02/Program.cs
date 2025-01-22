67ef56a312588b059703f3cff9c8ca9bdb3a7971
Nathan Jackson <nathanrjackson30@gmail.com>
2025-01-21 18:53:41 -0700

    finished w03 prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Owner";
+        job1._startYear = "1905";
+        job1._endYear = "Present";
+
+        Job job2 = new Job();
+        job2._company = "Ford";
+        job2._jobTitle = "Owner";
+        job2._startYear = "1593";
+        job2._endYear = "Present";
+
+        Resume myResume = new Resume();
+        myResume._name = "Nathan Jackson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
