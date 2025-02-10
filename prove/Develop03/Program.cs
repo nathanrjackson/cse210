746d08abc8cdcbb48c46adb7dfaf77f83a3b5117
Nathan Jackson <nathanrjackson30@gmail.com>
2025-02-10 14:29:41 -0700

    Started stopwatch creativity addition

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,10 +1,13 @@
+using System.Diagnostics;
+
 class Program
 {
     static void Main(string[] args)
     {
         Reference reference = new Reference("Moroni", 10, 4, 5);
         Scripture scripture = new Scripture("And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, if these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, he will manifest the truth of it unto you, by the power of the Holy Ghost. And by the power of the Holy Ghost ye may know the truth of all things.", reference);
-
+        Stopwatch stopwatch = new Stopwatch();
+        
         Console.Clear();
         Console.WriteLine(scripture);
 
