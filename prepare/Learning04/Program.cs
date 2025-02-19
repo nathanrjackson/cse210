c155aba674554dda00145a8f78103b40204bc36b
Nathan Jackson <nathanrjackson30@gmail.com>
2025-02-19 09:12:44 -0700

    Finished W07 Prepare: Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine();
+        
+        Assignment a1 = new Assignment("Nathan Jackson", "Programming");
+        Console.WriteLine(a1.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment a2 = new MathAssignment("Brother Comeau", "Addition Class", "10.0", "1-100");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment a3 = new WritingAssignment("Russell Russell", "Christian History", "Why the Restoration?");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
+        Console.WriteLine();
     }
 }
