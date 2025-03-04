e96499b9f823dc5c2f77d7c05c1ee284fe652478
Nathan Jackson <nathanrjackson30@gmail.com>
2025-03-04 13:26:18 -0700

    Completed polymorphism preparation activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,21 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 5);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Console.WriteLine();
+
+        Rectangle rectangle = new Rectangle("blue", 5, 3);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Console.WriteLine();
+
+        Circle circle = new Circle("green", 5);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
     }
 }
